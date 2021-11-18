b271f327799e1af27798e9b86bb442e68a51f88f
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-18 11:44:05 +0000

    AM, 2nd commit main

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,12 @@
+
 public class Runner {
+
+    public static void main(String[] args){
+        Singleton singleton1 = Singleton.getInstance();
+        System.out.println(singleton1.getData());
+        Singleton singleton2 = Singleton.getInstance();
+        singleton2.setData("The data has changed..");
+        Singleton singleton3 = Singleton.getInstance();
+        System.out.println(singleton3.getData());
+    }
 }
--- src/main/java/Singleton.java
+++ src/main/java/Singleton.java
@@ -1,2 +1,25 @@
+
 public class Singleton {
+
+    private static Singleton instance = null;
+    private String data;
+
+    private Singleton(){
+        this.data = "Hello, I am part of the singleton class.";
+    }
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public void setData(String newData){
+        this.data = newData;
+    }
 }
